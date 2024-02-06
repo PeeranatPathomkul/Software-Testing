28a397bf7848475f990c89196e7c4dc63748a978
Peeranat Pathomkul <focus.peeranat@gmail.com>
2024-02-07 02:10:04 +0700

    Add new EX3

---
+++ software_testing2/caesar_test/caesar_test.py
@@ -0,0 +1,9 @@
+from software_testing2.caesar_Cipher.caesar_Cipher import caesarCipher
+import unittest
+
+class CaesarTest(unittest.TestCase):
+    def test_caesar_num(self):
+        text = 'AAAA'
+        num_k = 5
+        result = caesarCipher(text,num_k)
+        self.assertEqual(result,'HHHH')
