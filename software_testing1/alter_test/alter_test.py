7ac66075bc1638221802b7b2121a0b58db9eda49
Peeranat Pathomkul <focus.peeranat@gmail.com>
2024-02-11 00:32:49 +0700

    Add Final TastCase2

--- software_testing1/alter_test/alter_test.py
+++ software_testing1/alter_test/alter_test.py
@@ -17,5 +17,33 @@ class AlterTest(unittest.TestCase):
         num_of_alter_char = alternatingChar(text)
         self.assertEqual(num_of_alter_char, '1')
 
+    def test_alter_manyChar_num(self):
+        text = '''AABBABABBBBBABBABABBBBABBABABABBABBABBBBAAABBBBBBBBBBBABBBBBBBABBBBBBBBBBBBABBABBBBAABBBBBAAAABBBBBB'''
+        num_of_alter_char = alternatingChar(text)
+        self.assertEqual(num_of_alter_char, '62')
+    
+    def test_alter_11111_num(self):
+        text = '11111'
+        num_of_alter_char = alternatingChar(text)
+        self.assertEqual(num_of_alter_char, '4')
+    
+    def test_alter_1234_num(self):
+        text = '1234'
+        num_of_alter_char = alternatingChar(text)
+        self.assertEqual(num_of_alter_char, '0')
+    
+    def test_alter_special_char1_num(self):
+        text = '#######'
+        num_of_alter_char = alternatingChar(text)
+        self.assertEqual(num_of_alter_char, '6')
+    
+    def test_alter_special_char2_num(self):
+        text = '#@$()**'
+        num_of_alter_char = alternatingChar(text)
+        self.assertEqual(num_of_alter_char, '1')
+    
+
+
+
     
 
